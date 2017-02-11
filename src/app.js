92547e4728d893e23cecf5fee2dcafd3daa5c2e6
Alex <oo.shcherbakov@gmail.com>
2017-02-11 11:41:35 -0500

    Add redux thunk

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import firebase from 'firebase';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCsJ1hF-rEEB22CE07rETPTXKbqyCQndNM",
+      authDomain: "manager-792a3.firebaseapp.com",
+      databaseURL: "https://manager-792a3.firebaseio.com",
+      storageBucket: "manager-792a3.appspot.com",
+      messagingSenderId: "66529146316"
+    })
+  }
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm>
         </LoginForm>
       </Provider>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Card, CardSection, Input, Button } from './common';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 class LoginForm extends Component {
 
@@ -11,6 +11,11 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
     this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+    this.props.loginUser({ email, password });
   }
 
   render() {
@@ -36,7 +41,7 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log In
           </Button>
         </CardSection>
@@ -51,7 +56,6 @@ const mapStateToProps = state => {
     password: state.auth.password
   }
 }
-export default connect(
-    mapStateToProps, 
-    { emailChanged, passwordChanged }
-  )(LoginForm);
+export default connect(mapStateToProps, 
+  { emailChanged, passwordChanged, loginUser }
+)(LoginForm);
